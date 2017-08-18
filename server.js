7831af64385d6911e23a36f8d4504ac899f480ab
kasriniv <kasriniv@users.noreply.github.com>
2017-08-18 14:27:10 -0400

    kavitha changes

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const app = express();
 //console.log(process.env)
 
 var healthy=true;
+//this is for JSON const bodyParser = require('body-parser');
+
+var bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+app.use(bodyParser.xml());
+//THIS IS IF BODY IS JSON, IN MY CASE IT IS XML app.use(bodyParser.json());
+
 
 
 app.get('/', function (req, res) {
+  console.log(req.headers);
   res.send('Hello world v.1.1 ' + os.hostname() + '\n');
 });
 
@@ -28,7 +36,12 @@ app.get('/cancer', function (req, res) {
    res.send('Killed ' + os.hostname());
 });
 
+app.post('/kpost', function (req, res) {
+  console.dir(req.body);
+console.log(req.body);
 
+   res.send('post all good ' + os.hostname());
+});
 
 app.listen(PORT,'0.0.0.0');
 console.log('Running on http://localhost:' + PORT);
@@ -38,4 +51,3 @@ process.on('SIGTERM', function () {
     console.log('Cleanup.....');
     process.exit();
 });
-
